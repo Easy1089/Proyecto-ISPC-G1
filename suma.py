7ce7800555be33f714ac2f743936eba32a9f1302
Fabiola <lumediaweb@gmail.com>
2022-05-28 18:04:34 -0300

    Subida de archivo suma.py

---
+++ suma.py
@@ -0,0 +1,5 @@
+#Ejemplo de python suma de dos valores
+valor = int (input("Ingrese primer valor:"))
+valor1 = int (input ("Ingrese segundo valor:"))
+suma = valor + valor1
+print ("El valor de la suma es:", suma)
