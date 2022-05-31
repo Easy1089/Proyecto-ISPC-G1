dd93b668c7d449d7272403c4c86ecb901c2725e8
Fernando Aparicio <aparicio.fernando1986@gmail.com>
2022-05-31 16:26:44 -0300

    Los guarde

---
+++ datosDelGrupo.py
@@ -0,0 +1,56 @@
+import os
+clear = lambda: os.system("cls")
+clear()
+
+nombresAlumnos={
+    "Alumno 1":"Fabiola Benítez", 
+    "Alumno 2":"Fernando Aparicio",
+    "Alumno 3":"Enzo Mateo Antunez",
+    "Alumno 4":"Marcelo Apaz",
+    "Alumno 5":"Melisa Apaz",
+    "Alumno 6":"Analia Aguirre",
+    "Alumno 7":"Ana Eugenia Abregú",
+    "Alumno 8":"Martín Arias",
+    "Alumno 9":"Florencia Arato",
+    "Alumno 10":"Dayana Bernal"
+    }
+
+mailAlumnos={
+    "Alumno 1":"fabiolaben03@gmail.com",
+    "Alumno 2":"aparicio.fernando1986@gmail.com",
+    "Alumno 3":"mateoantunez0@gmail.com",
+    "Alumno 4":"Marceysoleapazvidela@gmail.com",
+    "Alumno 5":"melisaapaz@gmail.com",
+    "Alumno 6":"anii_aguirre@hotmail.com",
+    "Alumno 7":"euge.abregu.ea@gmail.com",
+    "Alumno 8":"ariasmartinlautaro@gmail.com",
+    "Alumno 9":"florencia280393@gmail.com",
+    "Alumno 10":"dayanadanielito07@gmail.com"
+    }
+
+usuarioDeGitHub={
+    "Alumno 1":"Easy1089", 
+    "Alumno 2":"Aristo1986",
+    "Alumno 3":"MateoAntunez7",
+    "Alumno 4":"Marceloapaz",
+    "Alumno 5":"melisaapaz",
+    "Alumno 6":"AnyAguirre",
+    "Alumno 7":"AEugenia89",
+    "Alumno 8":"Martonsioh",
+    "Alumno 9":"FlorA-93",
+    "Alumno 10":"Day2022"
+}
+
+print(nombresAlumnos)
+print()
+print()
+print("==========================================================================")
+print()
+print()
+print(mailAlumnos)
+print()
+print()
+print("==========================================================================")
+print()
+print()
+print(usuarioDeGitHub)
