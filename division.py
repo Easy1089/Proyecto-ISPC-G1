fbdf61936c2a006c2a8ff496939ea0fd6a1340a1
Marcelo Apaz <marceysoleapazvidela@gmail.com>
2022-05-31 20:08:19 -0300

    Commit de método división

---
+++ division.py
@@ -0,0 +1,5 @@
+#Ejemplo en python de dividir dos valores
+valor = int (input("Ingrese primer valor: "))
+valor1 = int (input ("Ingrese segundo valor:" ))
+div = valor / valor1
+print ("El valor de la división es: ", div)
