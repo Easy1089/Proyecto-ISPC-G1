4556867a2d9c154c9d178927b6abbcc421598bc3
Fabiola <lumediaweb@gmail.com>
2022-08-10 13:42:46 -0300

    Add existing file peluqueriacanina-script.sql corregido

--- pelucanina-script.sql.sql
+++ pelucanina-script.sql.sql
@@ -12,40 +12,42 @@ CREATE TABLE dueno (
 );
  
 CREATE TABLE perro (
-    id_perro INT UNSIGNED PRIMARY KEY,
+    id_perro INT PRIMARY KEY,
     nombre VARCHAR(25) NOT NULL,
     fecha_nacimiento DATE NOT NULL,
     sexo ENUM('H', 'M') NOT NULL,
-    FOREIGN KEY (id_perro) REFERENCES dueno(dni)
+    dni_dueno INT,
+    FOREIGN KEY (dni_dueno) REFERENCES dueno(dni)
     
 );
  
  CREATE TABLE historial (
-    id_historial INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id_historial INT AUTO_INCREMENT PRIMARY KEY,
     fecha DATE NOT NULL,
+    perro INT,
     FOREIGN KEY (perro) REFERENCES perro(id_perro),
     descripcion VARCHAR(100) NOT NULL,
     monto FLOAT UNSIGNED NOT NULL
 );
  /* dueno*/
 
-INSERT INTO persona VALUES ('79503962', 'Cristina', 'Lemke', 'Saturno', '669162534');
-INSERT INTO persona VALUES ('82842571', 'Ramón', 'Herzog', 'Urano', '626351429');
-INSERT INTO persona VALUES ('61142000', 'Esther', 'Spencer','Plutón', NULL);
-INSERT INTO persona VALUES ('46900725', 'Daniel','Pacocha', 'Andarax', '679837625');
-INSERT INTO persona VALUES ('85366986', 'Carmen', 'Streich', 'Almanzora', NULL);
-INSERT INTO persona VALUES ('73571384', 'Alfredo', 'Stiedemann', 'Guadalquivir', '950896725');
-INSERT INTO persona VALUES ('82937751', 'Manolo', 'Hamill', 'C/ Duero', '950263514');
-INSERT INTO persona VALUES ('80502866', 'Alejandro', 'Kohler', 'C/ Tajo', '668726354');
-INSERT INTO persona VALUES ('10485008', 'Antonio', 'Fahey', 'Sierra de los Filabres', NULL);
-INSERT INTO persona VALUES ('85869555', 'Guillermo', 'Ruecker','Sierra de Gádor', NULL);
-INSERT INTO persona VALUES ('04326833', 'Micaela', 'Monahan', 'C/ Veleta', '662765413');
-INSERT INTO persona VALUES ('11578526', 'Inma', 'Lakin', 'Picos de Europa', '678652431');
-INSERT INTO persona VALUES ('79221403', 'Francesca', 'Schowalter', 'Quinto pino', NULL);
-INSERT INTO persona VALUES ('79089577', 'Juan', 'Gutiérrez', 'Los pinos', '678652431');
-INSERT INTO persona VALUES ('41491230', 'Antonio', 'Domínguez', 'C/ Cabo de Gata', '626652498');
-INSERT INTO persona VALUES ('64753215', 'Irene', 'Hernández', 'C/ Zapillo', '628452384');
-INSERT INTO persona VALUES ('85135690', 'Sonia', 'Gea', 'C/ Mercurio', '678812017');
+INSERT INTO dueno VALUES ('79503962', 'Cristina', 'Lemke', 'Saturno', '669162534');
+INSERT INTO dueno VALUES ('82842571', 'Ramón', 'Herzog', 'Urano', '626351429');
+INSERT INTO dueno VALUES ('61142000', 'Esther', 'Spencer','Plutón', NULL);
+INSERT INTO dueno VALUES ('46900725', 'Daniel','Pacocha', 'Andarax', '679837625');
+INSERT INTO dueno VALUES ('85366986', 'Carmen', 'Streich', 'Almanzora', NULL);
+INSERT INTO dueno VALUES ('73571384', 'Alfredo', 'Stiedemann', 'Guadalquivir', '950896725');
+INSERT INTO dueno VALUES ('82937751', 'Manolo', 'Hamill', 'C/ Duero', '950263514');
+INSERT INTO dueno VALUES ('80502866', 'Alejandro', 'Kohler', 'C/ Tajo', '668726354');
+INSERT INTO dueno VALUES ('10485008', 'Antonio', 'Fahey', 'Sierra de los Filabres', NULL);
+INSERT INTO dueno VALUES ('85869555', 'Guillermo', 'Ruecker','Sierra de Gádor', NULL);
+INSERT INTO dueno VALUES ('04326833', 'Micaela', 'Monahan', 'C/ Veleta', '662765413');
+INSERT INTO dueno VALUES ('11578526', 'Inma', 'Lakin', 'Picos de Europa', '678652431');
+INSERT INTO dueno VALUES ('79221403', 'Francesca', 'Schowalter', 'Quinto pino', NULL);
+INSERT INTO dueno VALUES ('79089577', 'Juan', 'Gutiérrez', 'Los pinos', '678652431');
+INSERT INTO dueno VALUES ('41491230', 'Antonio', 'Domínguez', 'C/ Cabo de Gata', '626652498');
+INSERT INTO dueno VALUES ('64753215', 'Irene', 'Hernández', 'C/ Zapillo', '628452384');
+INSERT INTO dueno VALUES ('85135690', 'Sonia', 'Gea', 'C/ Mercurio', '678812017');
  
 /* perro */
 INSERT INTO perro VALUES (3303,'pelu', '2020/08/21', 'H','85135690');
@@ -59,7 +61,6 @@ INSERT INTO perro VALUES (2325,'vicente','2006/12/29', 'M','10485008');
 INSERT INTO perro VALUES (1406,'lola', '2007/11/30', 'H','80502866');
 INSERT INTO perro VALUES (5698,'luna', '2003/10/04', 'H','82937751');
 INSERT INTO perro VALUES (5263,'benji', '2022/08/06', 'M','73571384');
-INSERT INTO perro VALUES (5698,'arena', '2014/08/10', 'H','85366986');
 INSERT INTO perro VALUES (7898,'pata', '2022/11/18', 'H','46900725');
 INSERT INTO perro VALUES (6542,'huesos', '2021/01/22', 'M','61142000');
 INSERT INTO perro VALUES (3254,'enana', '2020/05/28', 'H','82842571');
